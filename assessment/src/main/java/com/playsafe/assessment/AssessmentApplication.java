a2adcb4c08d4f65813360e7b3506439a8e654164
David Umera <davume@gmail.com>
2020-04-25 10:59:34 +0200

    Endpoint performance added

--- assessment/src/main/java/com/playsafe/assessment/AssessmentApplication.java
+++ assessment/src/main/java/com/playsafe/assessment/AssessmentApplication.java
@@ -2,6 +2,11 @@ package com.playsafe.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @SpringBootApplication
 public class AssessmentApplication {
@@ -10,4 +15,16 @@ public class AssessmentApplication {
         SpringApplication.run(AssessmentApplication.class, args);
     }
 
+    @Component
+    public class LogTimeFilter implements Filter {
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+            long startTime = System.currentTimeMillis();
+            chain.doFilter(request, response);
+            long duration = System.currentTimeMillis() - startTime;
+            String url = ((HttpServletRequest)request).getRequestURL().toString();
+            System.out.println("Endpoint: "+ url + " >>>" + duration + " ms");
+        }
+    }
 }
